b55721d5537a1f435da4967123f4dbc288b4c87c
Jose Tinajero <jose.tinajero@epn.edu.ec>
2017-05-16 16:22:41 -0500

    Typescript, variables,interfaces,funciones

---
+++ 03 TypeScript/hola.js
@@ -0,0 +1,45 @@
+//let holaMundo = `Que tal la clase`;
+//holaMundo
+//console.log("Hola Mundo");
+var soltero = true; //false
+//Number
+var novias = 4;
+//String
+var frase = "You are happy";
+var otraFrase = "El numero de novias es : " + (novias + 1);
+//arrays
+var notas = [1, 2, 3, 4]; //[]
+var jaladas = [1, 2, 3]; //[]
+//any sirve para utilizar las ventajas de javascript en typescript
+var nombre = "asdasd";
+nombre = 12;
+//crear nuestro propios tipos de datos
+var usuario = {
+    nombre: "Andres",
+    apellido: "Tinajero"
+};
+//escribir clases
+/* Esto es lo mismo que lo de abajo, la diferencia es que el codigo autolaticamente hace el match
+class Persona{
+    nombre:string = "Nom"; //publico por defecto
+    private apellido:string;
+    public fechaNacimiento:Date;
+    constructor(nombre?:string,
+                apellido?:string,
+                fechaNacimiento?:Date){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+}
+*/
+var Persona = (function () {
+    function Persona(nombre, apellido, fechaNacimiento) {
+        this.nombre = "Nom"; //publico por defecto
+    }
+    Persona.prototype.imprimirPersona = function () {
+        console.log(this.nombre, this.apellido, this.fechaNacimiento);
+    };
+    return Persona;
+}());
+var andres = new Persona(); //instanciamos la clase persona
