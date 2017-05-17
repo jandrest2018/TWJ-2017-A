7826c918951bb37a7522f234b8b7becb0a7a8a7e
Jose Tinajero <jose.tinajero@epn.edu.ec>
2017-05-17 14:34:41 -0500

    Mi primera aplicación en angular

--- 04 Angular/a-web/src/app/app.component.ts
+++ 04 Angular/a-web/src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'TWJ-raiz'
-  templateUrl: './app.component.html',
+  selector: 'twj-raiz',
+  template: `
+    <h1>Hola</h1>
+
+  `,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'FELIZ!';
+  usuario:UsuarioInt = {
+    nombre:"Andres",
+    apellido:"Tinajero"
+  }
+
+  constructor(){
+    console.log("Constructor");
+    this.holaMundo();
+    this.saludar(this.usuario.nombre,
+      this.usuario.apellido
+    );
+  }
+
+  holaMundo(){
+    console.log("Hola mundo");
+  }
+  saludar(nombre:string,
+          apellido?:string):string{
+    return `Hola ${nombre} ${apellido}`;
+  }
+
+}
+
+interface UsuarioInt{
+  nombre:string,
+  apellido:string
 }
